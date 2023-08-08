b835a89147825615c5936e69ffdd1f472e0ca8e6
prematal2828 <prematal2828@gmail.com>
2023-08-08 16:22:14 +0600

    I have created a logo but it is not good

--- src/app/(viewer)/page.tsx
+++ src/app/(viewer)/page.tsx
@@ -4,12 +4,14 @@ import ColorPaletteCard from "../components/ColorPaletteCard";
 
 export default function HomePage() {
   return (
-    <div className="container grid grid-cols-2 md:grid-cols-3 lg:grid-cols-5 xl:grid-cols-6 gap-5">
-      {Array(100)
-        .fill(0)
-        .map((_, idx) => {
-          return <ColorPaletteCard key={idx} />;
-        })}
-    </div>
+    <main>
+      <div className="container grid grid-cols-2 md:grid-cols-3 lg:grid-cols-5 xl:grid-cols-6 gap-5">
+        {Array(100)
+          .fill(0)
+          .map((_, idx) => {
+            return <ColorPaletteCard key={idx} />;
+          })}
+      </div>
+    </main>
   );
 }
